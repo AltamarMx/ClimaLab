6b6355e5d0f32fa80fabbd596bedb7c1968a7a9d
altamarmx <gbv@ier.unam.mx>
2025-07-11 13:05:14 -0600

    confort implementador, sever explorer refactorizado

--- app_explorer.py
+++ app_explorer.py
@@ -11,12 +11,14 @@ from components.panels import (
     panel_trayectoriasolar,
     panel_fotovoltaica,
     panel_eolica,
-    panel_confort,
 )
+from components.thermal_comfort_ui import panel_confort
 from components.sun_path_server import sun_path_server
+from components.thermal_comfort_ui import panel_confort
+from components.thermal_comfort_server import thermal_comfort_server
+from components.explorer_server import explorer_server
 
 # from utils.data_processing import load_esolmet_data
-from utils.plots import plot_explorer_matplotlib
 
 
 app_ui = ui.page_fillable(
@@ -46,54 +48,9 @@ app_ui = ui.page_fillable(
 
 def server(input, output, session):
 
-
-    @output
-    @render.plot
-    def plot_explorer():
-        fechas = input.fechas()
-        if fechas is not None:
-            return plot_explorer_matplotlib(fechas)
-        return plot_explorer_matplotlib()
-
-    def _query_df(fechas):
-        con = duckdb.connect(db_name)
-        q = f"""
-        SELECT *
-          FROM lecturas
-         WHERE date >= TIMESTAMP '{fechas[0]}'
-           AND date <= TIMESTAMP '{fechas[1]}'
-         ORDER BY date
-        """
-        df = con.execute(q).fetchdf().pivot(index="date", columns="variable", values="value")
-        con.close()
-        return df
-
-    @output
-    @render.download(filename="ClimaLab.parquet", media_type="application/x-parquet")
-    def dl_parquet():
-        fechas = input.fechas()
-        if fechas is None:
-            return
-        df = _query_df(fechas)
-        with io.BytesIO() as buf:
-            df.to_parquet(buf, index=True, engine="pyarrow")
-            buf.seek(0)
-            yield buf.getvalue()
-
-    @output
-    @render.download(filename="ClimaLab.csv", media_type="text/csv")
-    def dl_csv():
-        fechas = input.fechas()
-        if fechas is None:
-            return
-        df = _query_df(fechas)
-        with io.StringIO() as buf:
-            df.to_csv(buf, index=True)
-            buf.seek(0)
-            yield buf.getvalue()
-
-            
+    explorer_server(input,output,session)
     sun_path_server(input, output, session)
+    thermal_comfort_server(input, output, session)
 
 
 
---
+++ components/explorer_server.py
@@ -0,0 +1,53 @@
+
+from shiny import App, ui, render
+import shinyswatch
+import duckdb
+from utils.plots import plot_explorer_matplotlib
+
+
+def explorer_server(input, output, session):
+    @output
+    @render.plot
+    def plot_explorer():
+        fechas = input.fechas()
+        if fechas is not None:
+            return plot_explorer_matplotlib(fechas)
+        return plot_explorer_matplotlib()
+
+    def _query_df(fechas):
+        con = duckdb.connect(db_name)
+        q = f"""
+        SELECT *
+          FROM lecturas
+         WHERE date >= TIMESTAMP '{fechas[0]}'
+           AND date <= TIMESTAMP '{fechas[1]}'
+         ORDER BY date
+        """
+        df = con.execute(q).fetchdf().pivot(index="date", columns="variable", values="value")
+        con.close()
+        return df
+
+    @output
+    @render.download(filename="ClimaLab.parquet", media_type="application/x-parquet")
+    def dl_parquet():
+        fechas = input.fechas()
+        if fechas is None:
+            return
+        df = _query_df(fechas)
+        with io.BytesIO() as buf:
+            df.to_parquet(buf, index=True, engine="pyarrow")
+            buf.seek(0)
+            yield buf.getvalue()
+
+    @output
+    @render.download(filename="ClimaLab.csv", media_type="text/csv")
+    def dl_csv():
+        fechas = input.fechas()
+        if fechas is None:
+            return
+        df = _query_df(fechas)
+        with io.StringIO() as buf:
+            df.to_csv(buf, index=True)
+            buf.seek(0)
+            yield buf.getvalue()
+
