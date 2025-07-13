fb1051878d8400e2d3a391a698db2108953f1cc9
Guillermo Barrios del Valle <gbv@ier.unam.mx>
2025-07-13 11:17:35 -0600

    Use date range for seasonal wind roses

--- components/wind_power_server.py
+++ components/wind_power_server.py
@@ -113,8 +113,16 @@ def wind_power_server(input, output, session):
         if n_clicks is None or n_clicks == 0:
             return None
         with reactive.isolate():
-            start_year, end_year = input.season_year_range()
-        df = esolmet.loc[f"{start_year}-01-01": f"{end_year}-12-31"]
+            start_date_str, end_date_str = input.season_date_range()
+        max_date = esolmet.index.max()
+        min_date = esolmet.index.min()
+        start_last_year = max(max_date - pd.DateOffset(years=1) + pd.Timedelta(days=1), min_date)
+        start_date = pd.to_datetime(start_date_str)
+        end_date = pd.to_datetime(end_date_str)
+        if start_date == start_last_year and end_date == max_date:
+            df = esolmet.loc[start_last_year:end_date]
+        else:
+            df = esolmet.loc[start_date:end_date]
         return create_seasonal_wind_roses_by_speed_plotly(df)
 
     @render_widget
